19b539bb1b8ea047b35a48afd1d2ca6e515be810
Jarod Kong <1265598443@qq.com>
2022-01-05 09:56:36 +0800

    fixed: 本地修正 : subqueryContext NPE

--- shardingsphere-infra/shardingsphere-infra-binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/select/subquery/engine/SubqueryTableContextEngine.java
+++ shardingsphere-infra/shardingsphere-infra-binder/src/main/java/org/apache/shardingsphere/infra/binder/segment/select/subquery/engine/SubqueryTableContextEngine.java
@@ -40,10 +40,12 @@ public final class SubqueryTableContextEngine {
      */
     public Collection<SubqueryTableContext> createSubqueryTableContexts(final SelectStatementContext subqueryContext, final String alias) {
         Collection<SubqueryTableContext> result = new LinkedList<>();
-        List<String> columnNames = subqueryContext.getProjectionsContext().getExpandProjections().stream()
-                .filter(each -> each instanceof ColumnProjection).map(each -> ((ColumnProjection) each).getName()).collect(Collectors.toList());
-        for (String each : subqueryContext.getTablesContext().getTableNames()) {
-            result.add(new SubqueryTableContext(each, alias, columnNames));
+        if (subqueryContext != null && subqueryContext.getProjectionsContext() != null) {
+            List<String> columnNames = subqueryContext.getProjectionsContext().getExpandProjections().stream()
+                    .filter(each -> each instanceof ColumnProjection).map(each -> ((ColumnProjection) each).getName()).collect(Collectors.toList());
+            for (String each : subqueryContext.getTablesContext().getTableNames()) {
+                result.add(new SubqueryTableContext(each, alias, columnNames));
+            }
         }
         return result;
     }
